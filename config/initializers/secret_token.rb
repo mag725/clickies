fa50e422670ce5d2b895974e2bb69bda54932a82
Matthew Gilk <matt@sweetperk.com>
2012-05-27 10:44:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clickies::Application.config.secret_token = 'df50a5333ad653c866e788d64d3470191fa7a180ea69a8f052b3782a1e77d9e4251839d816ca40ae2b8284c4553d14857bb004d8b323a2fd1011e130a7843ec1'
